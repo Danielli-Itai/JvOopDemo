15e317bc160cdd7f4fd378c736d99e0aa403d0f7
Itai Danielli <Danielli.Itai@gmail.com>
2021-07-09 12:58:31 +0300

    Exercises 1,2 template files.

---
+++ Ex1.Hello/src/HelloMain.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What is your name ?");
+		
+		//// Enter your code here !
+		////
+
+		return;
+	}
+
+}
---
+++ Ex2.Loops/src/a_proc_exec_flow/MainExecFlow.java
@@ -0,0 +1,18 @@
+package a_proc_exec_flow;
+
+import java.io.IOException;
+
+
+public class MainExecFlow {
+	//// Enter your code here !
+	////
+
+	
+	//Prints Hello World!
+	public static void main (String[] args) throws IOException
+	{
+		//// Enter your code here !
+		////
+	}
+
+}
